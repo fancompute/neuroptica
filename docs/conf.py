7685045298f18f54120bc7fc57991d2457423811
Ben Bartlett <benjamincbartlett@gmail.com>
2019-03-06 15:58:18 -0800

    update doc conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,20 @@
 #
 import os
 import sys
+from unittest.mock import MagicMock
 
 sys.path.insert(0, os.path.abspath('..'))
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+
+MOCK_MODULES = ['numpy', 'scipy', 'numba']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 # -- Project information -----------------------------------------------------
 
